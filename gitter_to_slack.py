6bc5a12ae8e677c9441b44379eb409eaf2c3b0ab
Alexandre kurth <alexandre.kurth@gmail.com>
2016-10-21 18:48:01 +0200

    first working mirror

--- gitter_to_slack.py
+++ gitter_to_slack.py
@@ -1,25 +1,26 @@
 import json
 import requests
+from slackclient import SlackClient
 
-def list_channels():
+def slack_channels(slack_client, filtr):
     channels_call = slack_client.api_call("channels.list")
+    channels_to_filter = {}
     if channels_call.get('ok'):
-        return channels_call['channels']
-    return None
+        channels = channels_call['channels']
+        for channel in channels:
+            channel_name = channel["name"]
+            if channel_name.startswith(filtr):
+                channels_to_filter[channel["name"]] = channel["id"]
+    return channels_to_filter
 
-def send_slack_message(channel_id, message):
+def send_slack_message(slack_client, channel_id, user, message, avatar):
     slack_client.api_call(
         "chat.postMessage",
         channel=channel_id,
         text=message,
-        username='pythonbot',
+        username=user,
         icon_emoji=':robot_face:'
     )
-
-
-def mirror_gitter_to_slack(data):
-    # urls, sent, unread, html, mentions, issues, id, meta, readBy, fromUser, v, text
-    print(data['text'])
 
 def gitter_rooms(token, filtr):
     r = requests.get('https://api.gitter.im/v1/rooms?access_token={}'.format(token))
@@ -28,42 +29,51 @@ def gitter_rooms(token, filtr):
     for room in rooms:
         room_name = room["name"]
         if room_name.startswith(filtr):
-            rooms_to_mirror[room["id"]] = room["name"]
+            rooms_to_mirror[room["name"]] = room["id"]
     return rooms_to_mirror
 
-def listen_from_gitter(token):
-    r = requests.get('https://api.gitter.im/v1/rooms?access_token={}'.format(token))
-    rooms = json.loads(r.text)
+def mirror_to_slack(slack_channel_id, data, slack_client):
+    # urls, sent, unread, html, mentions, issues, id, meta, readBy, fromUser, v, text
+    msg = data['text']
+    name = data['fromUser']['displayName']
+    avatar =  data['fromUser']['avatarUrl']
+    send_slack_message(slack_client, slack_channel_id, name, msg, avatar)
 
-    rooms_to_mirror = {}
-    for room in rooms:
-        room_name = room["name"]
-        if room_name.startswith('gloubi'):
-            rooms_to_mirror[room["id"]] = room["name"]
-    print(rooms_to_mirror)
-    room_ids = [room_id for room_id in rooms_to_mirror]
 
-    # draft with only one room
-    room_id = room_ids[0]
-    url = 'https://stream.gitter.im/v1/rooms/{}/chatMessages'.format(room_id)
+
+
+def listen_gitter_post_slack(token, slack_client, gitter_room_id, slack_channel_id):
+    url = 'https://stream.gitter.im/v1/rooms/{}/chatMessages'.format(gitter_room_id)
     headers = {'Authorization': 'Bearer ' + token}
     r = requests.get(url, headers=headers, stream=True)
     for line in r.iter_lines():
         if line.strip():
             data = json.loads(line.decode('utf8'))
-            # print(data)
-            mirror_to_slack(data)
+            mirror_to_slack(slack_channel_id, data, slack_client)
 
 
 if __name__ == '__main__':
 
-
-
     with open('token_gitter', 'r') as tokenfile_gitter:
         with open('token_slack', 'r') as tokenfile_slack:
+            
             token_gitter = tokenfile_gitter.read().strip()
             token_slack = tokenfile_slack.read().strip()
-
-
+            slack_client = SlackClient(token_slack)
+          
+            #gitter_rooms = gitter_rooms(token_gitter, 'gloubi')
             gitter_rooms = gitter_rooms(token_gitter, 'ethereum')
-            print(gitter_rooms)
+            slack_channels = slack_channels(slack_client, 'test')
+            
+            # gitter: slack
+            mirror = [
+                #['gloubi/Lobby','test_bot'], 
+                ['ethereum/solidity','test_bot'],
+            ]
+
+            for gitter_room, slack_channel in mirror:
+                listen_gitter_post_slack(token_gitter, slack_client, gitter_rooms[gitter_room], slack_channels[slack_channel])
+
+
+
+ubi/Lobby
